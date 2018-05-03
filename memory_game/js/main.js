241d9b88418fa863aa873d5dd78ab9591187010c
McGovern <AM68254@ad2.prod>
2018-05-03 19:18:54 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,40 @@
 
-var cards = ["queen", "queen", "king", "king"]
+var cards = [
+    {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png",
+        },
+        {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png",
+        },
+        {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+        },
+        {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+        },
+        ];
 var cardsInPlay = []
 var checkForMatch = function () { 
-    if(cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+        if(cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
-        } else{
+        } 
+        else{
         alert("Sorry, try again."); }
-}
+        }
 var flipCard = function (cardId) {
-checkForMatch();
-
-cardsInPlay.push(cards[cardId]);
-console.log("User flipped" + " " + cards[cardId])
+    cardsInPlay.push(cards[cardId].rank);
+    checkForMatch();
+console.log("User flipped" + " " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 };
 flipCard(0)
 flipCard(2)
