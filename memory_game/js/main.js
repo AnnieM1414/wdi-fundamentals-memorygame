69abc498f6b4d1ac1c55078ed63c3cb346778bb9
McGovern <AM68254@ad2.prod>
2018-05-03 21:05:07 -0400

    created logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,46 +1,58 @@
 
 var cards = [
     {
-    rank: "queen",
-    suit: "hearts",
-    cardImage: "images/queen-of-hearts.png",
-        },
-        {
-    rank: "queen",
-    suit: "diamonds",
-    cardImage: "images/queen-of-diamonds.png",
-        },
-        {
-    rank: "king",
-    suit: "hearts",
-    cardImage: "images/king-of-hearts.png",
-        },
-        {
-    rank: "king",
-    suit: "diamonds",
-    cardImage: "images/king-of-diamonds.png",
-        },
-        ];
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png",
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png",
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png",
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png",
+    },
+];
 var cardsInPlay = []
-var checkForMatch = function () { 
-        if(cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function () {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
         alert("You found a match!");
-        } 
-        else{
-        alert("Sorry, try again."); }
-        }
+    }
+    else {
+        alert("Sorry, try again.");
+    }
+};
 var flipCard = function (cardId) {
     cardsInPlay.push(cards[cardId].rank);
-    checkForMatch();
-console.log("User flipped" + " " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+    console.log("User flipped" + " " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
 };
-flipCard(0)
-flipCard(2)
+var createBoard = function () {
+    for (var i = 0; i < cards.length; i++) {
+        console.log(cards[i]);
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src',"images/back.png");
+        cardElement.setAttribute('data-id', i);
+
+    }
+}
 
 
 
 
-console.log(cardsInPlay);
+
+flipCard(0);
+flipCard(2);
 
